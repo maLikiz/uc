19e40c0faed246b8e180052282aaf6c26eeb7a8a
Nikita Malykh <malixzu@gmail.com>
2019-07-12 21:19:25 +0500

    Remove jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,68 @@
-$(document).ready(function(){
-$('.gift').click(function(valtable){
-$('.gift').removeClass('active')
-$(this).addClass('active');
-}); 
+window.onload = function() {
+  /* Tools functions */
+  function $(query) {
+    return document.querySelector(query);
+  }
 
-function contH() {
-var sbh = $('.sidebar').height();
-$('.content').css({'min-height':sbh+'px'})
-$('.game_week img').on("load", function() {
-var sbh = $('.sidebar').height();
-$('.content').css({'min-height':sbh+'px'})
-});
-}
+  function $$(query) {
+    return document.querySelectorAll(query);
+  }
 
-$('.expander').click(function(){
-$('.top_nav').stop(true).slideToggle()
-});
-$('.expander2').click(function(){
-$('.nav_games').stop(true).slideToggle()
-});
-function expander() {
-var hH = $('header').height();
-var width = $(window).width();
-if (width > 752) {
-} else {
-$('.bg_header').css({'height':50+1*hH+'px'});
-}
-}
+  /* Unit functions */
+  function toggleExpand(element) {
+    let display = 'block';
 
+    if (element.style.display === 'block') {
+      display = 'none';
+    }
 
-$(window).scroll(function () {
-if ($(this).scrollTop() > 0) {
-$('.btn-to-top').show(); }
-else {
-$('.btn-to-top').hide();
-}
-});
-$('.btn-to-top').click(function () {
-$('body,html').animate({
-scrollTop: 0
-}, 400);
-return false;
-});
+    element.style.display = display;
+  }
 
-$('.redirect').click(function(){
-document.location.href = 'https://ucmirror.com/';
-});
+  /* Elements */
+  const giftElements = $$('.gift');
+  const btnToTopElement = $('.btn-to-top');
 
-});
+  /* Window events */
+  window.onscroll = function() {
+    display = 'none';
+
+    if (this.scrollY > 0) {
+      display = 'block';
+    }
+
+    btnToTopElement.style.display = display;
+  };
+
+  /* Events */
+  giftElements.forEach(giftElement => {
+    giftElement.onclick = function() {
+      giftElements.forEach(giftElementEach => {
+        giftElementEach.classList.remove('active');
+      });
+
+      this.classList.add('active');
+    };
+  });
+
+  $('.expander').onclick = function() {
+    toggleExpand($('.top_nav'));
+  };
+
+  $('.expander2').onclick = function() {
+    toggleExpand($('.nav_games'));
+  };
+
+  btnToTopElement.onclick = function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  $$('.redirect').forEach(redirect => {
+    redirect.onclick = function() {
+      document.location.href = 'https://ucmirror.com/';
+    };
+  });
+};
